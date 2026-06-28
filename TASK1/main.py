36d0d0fbab0c46793ac734d4682c233b69cbf26e
ihtisham-12 <baberihtasham640@gmail.com>
2026-06-28 11:57:07 +0500

    task 1 completed

---
+++ TASK1/main.py
@@ -0,0 +1,89 @@
+import json
+import unittest
+import datetime
+
+with open("data-1.json", "r", encoding="utf-8-sig") as f:
+    jsonData1 = json.load(f)
+
+with open("data-2.json", "r", encoding="utf-8-sig") as f:
+    jsonData2 = json.load(f)
+
+with open("data-result.json", "r", encoding="utf-8-sig") as f:
+    jsonExpectedResult = json.load(f)
+
+
+def convertFromFormat1(jsonObject):
+    location = jsonObject["location"].split("/")
+
+    return {
+        "deviceID": jsonObject["deviceID"],
+        "deviceType": jsonObject["deviceType"],
+        "timestamp": jsonObject["timestamp"],
+        "location": {
+            "country": location[0],
+            "city": location[1],
+            "area": location[2],
+            "factory": location[3],
+            "section": location[4]
+        },
+        "data": {
+            "status": jsonObject["operationStatus"],
+            "temperature": jsonObject["temp"]
+        }
+    }
+
+
+def convertFromFormat2(jsonObject):
+    dt = datetime.datetime.strptime(
+        jsonObject["timestamp"],
+        "%Y-%m-%dT%H:%M:%S.%fZ"
+    )
+
+    dt += datetime.timedelta(hours=5)
+
+    timestamp = int(dt.timestamp() * 1000)
+
+    return {
+        "deviceID": jsonObject["device"]["id"],
+        "deviceType": jsonObject["device"]["type"],
+        "timestamp": timestamp,
+        "location": {
+            "country": jsonObject["country"],
+            "city": jsonObject["city"],
+            "area": jsonObject["area"],
+            "factory": jsonObject["factory"],
+            "section": jsonObject["section"]
+        },
+        "data": jsonObject["data"]
+    }
+
+
+def main(jsonObject):
+    if jsonObject.get("device") is None:
+        return convertFromFormat1(jsonObject)
+    return convertFromFormat2(jsonObject)
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_sanity(self):
+        result = json.loads(json.dumps(jsonExpectedResult))
+        self.assertEqual(result, jsonExpectedResult)
+
+    def test_dataType1(self):
+        self.assertEqual(
+            main(jsonData1),
+            jsonExpectedResult,
+            "Converting from Type 1 failed"
+        )
+
+    def test_dataType2(self):
+        self.assertEqual(
+            main(jsonData2),
+            jsonExpectedResult,
+            "Converting from Type 2 failed"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
